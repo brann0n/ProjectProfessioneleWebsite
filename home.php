540cef6b6b20b54b9323aaa7d9580bf4acc7cf7e
RRemie <43609449+RRemie@users.noreply.github.com>
2018-10-16 14:30:57 +0200

    Standaard opmaak home pagina
    met div elementen

---
+++ home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<title>T' Veenhoog</title>
+	</head>
+	<body>
+	<div id="mainBody">
+		<div id="mainHeader">
+			<h1>Technische Hogeschool 'T Veenhoog</h1>
+		</div>
+	<div id="mainContent">
+		<div id="bgImage">
+			<div id="navigate">
+			</div>
+		</div>
+		<div id="loginButton">	
+		</div>
+		<div id="searchFunction">
+		</div>
+		<div id="newsLetterButton">
+		</div>
+	</div>
+	<div id="mainFooter">
+	</div>	
+	
+	</body>
+</html>
