884ad35b4ac2cddbf4d0bd50a65d66c7ad716d54
RRemie <43609449+RRemie@users.noreply.github.com>
2018-10-26 20:30:19 +0200

    Index afgemaakt
    Opmaak van de Index is grotendeels klaar.

--- index.php
+++ index.php
@@ -6,51 +6,89 @@
 		<title>T' Veenhoog</title>
 	</head>
 	<body>
-			<div class="logo">
+		<div class="logo">
 			<img src="image/logo.png" alt="logo"\>
 		</div>
+		<div class="language">
+			<h3><a href="index.php">Nederlands</a></h3>
+			<hr>
+			<h3><a href="indexE.php">English</a></h3>
+		</div>
+	
 	<div class="mainBody">
 
-		<div class="mainHeader">
-			<div class="h1Title">
-				<h1>Technische Hogeschool 'T Veenhoog</h1>
+			<div class="mainHeader">
+				<div class="h1Title">
+					<h1>Technische Hogeschool 'T Veenhoog</h1>
+				</div>
+			</div>
+		<div class="mainContent">
+			<div id="bgImage">
+				<div id="navigate">
+				<ul>
+					<li><a href="opleidingen.php">Opleidingen</a></li>
+					<li><a href="inschrijven.php">Inschrijven</a></li>
+					<li><a href="opendagen.php">Open dagen</a></li>
+					<li><a href="evenementen.php">Evenementen</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
+				</div>
+			</div>
+			<div id="loginButton">
+				<div class="buttonTextStyle">
+					<a href="login.php">Login</a>
+				</div>
+			</div>
+			<div id="searchFunction">
+				<div class="newsTextStyle">
+					<p>Vind jouw opleiding:
+						<select>
+							<option value="meme">Meme</option>
+							<option value="MEEM">MEEM</option>
+						</select>			
+					</p>	
+				</div>
+			</div>
+			<div id="newsLetterButton">
+				<div class="buttonTextStyle">
+					<a href="nieuwsbrief.php">Nieuwsbrief</a>
+				</div>
 			</div>
 		</div>
-	<div class="mainContent">
-		<div id="bgImage">
-			<div id="navigate">
-			<ul>
-				<li><a href="opleidingen.php">Opleidingen</a></li>
-				<li><a href="inschrijven.php">Inschrijven</a></li>
-				<li><a href="opendagen.php">Open dagen</a></li>
-				<li><a href="evenementen.php">Evenementen</a></li>
-				<li><a href="contact.php">Contact</a></li>
-			</ul>
+		<div class="mainFooter">
+			<div class="navFooterLeft">
+				<ul>
+					<li class="footerTopElement">T' Veenhoog:</li>
+					<li><a href="opleidingen.php">Opleidingen</a></li>
+					<li><a href="inschrijven.php">Inschrijven</a></li>
+					<li><a href="opendagen.php">Open dagen</a></li>
+					<li><a href="evenementen.php">Evenementen</a></li>
+					<li><a href="contact.php">Contact</a></li>
+				</ul>
 			</div>
-		</div>
-		<div id="loginButton">
-			<div class="buttonTextStyle">
-				<a href="login.php">Login</a>
+			<div class="navFooterCenter">
+				<ul>
+					<li class="footerTopElement">Onze partners:</li>
+					<li><a href="">(text)</a></li>
+					<li><a href="">(text)</a></li>
+					<li><a href="">(text)</a></li>
+					<li><a href="">(text)</a></li>
+				</ul>
 			</div>
-		</div>
-		<div id="searchFunction">
-			<div class="newsTextStyle">
-				<p>Vind jouw opleiding:
-					<select>
-						<option value="meme">Meme</option>
-						<option value="MEEM">MEEM</option>
-					</select>			
-				</p>	
+			<div class="navFooterRight">
+				<ul>
+					<li class="footerTopElementSocial">Volgs ons:</li>
+					<li><a href="https://www.facebook.com/"><img src="image/facebook.png" alt="Facebook"/></a>
+					<a href="https://twitter.com/"><img src="image/twitter.png" alt="Twitter"/></a>
+					<a href="https://youtube.com/"><img src="image/youtube.png" alt="Youtube"/></a>
+					</li>
+					<li class="socialStyle">
+					<a href="https://www.linkedin.com/"><img src="image/linkedin.png" alt="Linkedin"/></a>
+					<a href="https://instagram.com/"><img src="image/instagram.png" alt="Instagram"/></a>
+					</li>
+				</ul>
 			</div>
-		</div>
-		<div id="newsLetterButton">
-			<div class="buttonTextStyle">
-				<a href="nieuwsbrief.php">Nieuwsbrief</a>
-			</div>
-		</div>
+		</div>	
 	</div>
-	<div class="mainFooter">
-	</div>	
-	
 	</body>
 </html>
