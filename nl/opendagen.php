e01566366ac53d5799dc07e337794eecfb0249f3
NicktenCaat <43609554+NicktenCaat@users.noreply.github.com>
2018-11-02 14:40:15 +0100

    Update nl/opendagen.php

--- nl/opendagen.php
+++ nl/opendagen.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+session_start();
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -77,7 +75,8 @@ and open the template in the editor.
             </div>
         </div>
         <?php
-        // put your code here
+        // control login
+        
         ?>
     </body>
 </html>
