ddceeee532054a54c1504891679addb02807acfe
NicktenCaat <43609554+NicktenCaat@users.noreply.github.com>
2018-11-05 22:49:42 +0100

    Update en/index.php

--- en/index.php
+++ en/index.php
@@ -3,7 +3,7 @@
 session_start();
 ?>
 <!DOCTYPE html>
-<html lang="nl">
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="../style.css">
