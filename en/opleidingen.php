839519b360f9021a435d18a4465e7b7e4a565d75
brann0n <43607839+brann0n@users.noreply.github.com>
2018-11-08 12:34:21 +0100

    Fixed michiels pagina;s op BR tags enzo

--- en/opleidingen.php
+++ en/opleidingen.php
@@ -4,117 +4,129 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <link rel="icon" href="../image/logo.png">
-    <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="../style.css">
-    <title>T' Veenhoog</title>
-</head>
-<body>
-    <div class="centering">
-        <div class="logo">
-            <a href="index.php"><img src="../image/logo.png" alt="logo"></a>
-        </div>
-
-
-        <div class="mainBody">
-
-            <div class="mainHeader">
-                <div class="h1Title">
-                    <h1>Technische Hogeschool 'T Veenhoog</h1>
-                </div>
+    <head>
+        <link rel="icon" href="../image/logo.png">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="../style.css">
+        <title>T' Veenhoog</title>
+    </head>
+    <body>
+        <div class="centering">
+            <div class="logo">
+                <a href="index.php"><img src="../image/logo.png" alt="logo"></a>
             </div>
-            <div class="mainopleiding">
-                <div class="h2title"><h2>Here stands everything about our courses</h2></div>
-                <div class="box-opleiding">
-                    <div class="h2subtitle"><h2>Automotive</h2></div>
-                    <br>
-                    <p>
-                        The world of cars and technology is in one of its most exciting phases ever.
-                        Our future mobility requires solutions that are revolutionary.
-                    </p><br><p>
-                        Just think about the finiteness of fossil fuels and the emission of harmful substances.
-                        In that future we learn to deal more creatively with raw materials, we learn to look differently at the safety of every road user and we think of solutions that reduce traffic congestion.
-                        The good news is that the automotive world is already experimenting at top speed.
-                        With smart applications of ICT, electronics and lightweight construction, but also by the use of electric or hybrid drives.
-                    </p><br><p>
-                        The hero's in this story are the engineers from 'T Veenhoog.
-                        It is mainly the problem solvers who look further and understand that every technical solution
-                        fits into a complex picture in which mobility must be even smarter, cleaner, cheaper and more efficient.
-                        We call that 'complete vehicle thinking'. You learn that in 'T Veenhoog. Stap in en Drive your Future.
-                    </p>
-                    <a href="inschrijvenAutomotive.php">
-                        <div class="inschrijvenButton">	
-                            <h3>Apply for Automotive</h3>
-                        </div>
-                    </a>
+            <div class="mainBody">
+                <div class="mainHeader">
+                    <div class="h1Title">
+                        <h1>Technische Hogeschool 'T Veenhoog</h1>
+                    </div>
                 </div>
-                <div class="box-opleiding">
-                    <div class="h2subtitle"><h2>Mechatronica</h2></div>
-                    <br>
-                    <p>
-                        Mechatronics is a multidisciplinary combination of electrical engineering, mechanical engineering and technical informatics.
-                        In this course you are a logical thinker and a real doer.
-                        You consider how to make devices safer or more user-friendly and you design them.
-                        Think of an automated car wash, a smart production robot, or an advanced wheelchair or replacement robotic arm within the healthcare sector.
-                        Modern humanity can no longer do without mechatronics and there is a great need for well-trained mechatronics engineers.
-                        At 'T Veenhoog, we get to know the theory and practice and we train you to become a specialist in the systematic design of, among other things, control techniques for robot-controlled products and machines.
-                    </p>
-                    <a href="inschrijvenMechatronica.php">
-                        <div class="inschrijvenButton">	
-                            <h3>Apply for Mechatronics</h3>
+                <div class="mainopleiding">
+                    <div class="h2title"><h2>Here stands everything about our courses</h2></div>
+                    <div class="box-opleiding">
+                        <div class="h2subtitle">
+                            <h2>Automotive</h2>
                         </div>
-                    </a>    
+                        <p>
+                            The world of cars and technology is in one of its most exciting phases ever.
+                            Our future mobility requires solutions that are revolutionary.
+                        </p>
+                        <p>
+                            Just think about the finiteness of fossil fuels and the emission of harmful substances.
+                            In that future we learn to deal more creatively with raw materials, we learn to look differently at the safety of every road user and we think of solutions that reduce traffic congestion.
+                            The good news is that the automotive world is already experimenting at top speed.
+                            With smart applications of ICT, electronics and lightweight construction, but also by the use of electric or hybrid drives.
+                        </p>
+                        <p>
+                            The hero's in this story are the engineers from 'T Veenhoog.
+                            It is mainly the problem solvers who look further and understand that every technical solution
+                            fits into a complex picture in which mobility must be even smarter, cleaner, cheaper and more efficient.
+                            We call that 'complete vehicle thinking'. You learn that in 'T Veenhoog. Stap in en Drive your Future.
+                        </p>
+                        <a href="inschrijvenAutomotive.php">
+                            <div class="inschrijvenButton">	
+                                <h3>Apply for Automotive</h3>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="box-opleiding">
+                        <div class="h2subtitle"><h2>Mechatronica</h2></div>
+                        <br>
+                        <p>
+                            Mechatronics is a multidisciplinary combination of electrical engineering, mechanical engineering and technical informatics.
+                            In this course you are a logical thinker and a real doer.
+                            You consider how to make devices safer or more user-friendly and you design them.
+                            Think of an automated car wash, a smart production robot, or an advanced wheelchair or replacement robotic arm within the healthcare sector.
+                            Modern humanity can no longer do without mechatronics and there is a great need for well-trained mechatronics engineers.
+                            At 'T Veenhoog, we get to know the theory and practice and we train you to become a specialist in the systematic design of, among other things, control techniques for robot-controlled products and machines.
+                        </p>
+                        <a href="inschrijvenMechatronica.php">
+                            <div class="inschrijvenButton">	
+                                <h3>Apply for Mechatronics</h3>
+                            </div>
+                        </a>    
+                    </div>	
+                </div>
+                <div class="mainFooter">
+                    <div class="navFooterLeft">
+                        <ul>
+                            <li class="footerTopElement">T' Veenhoog:</li>
+                            <li><a href="opleidingen.php">Courses</a></li>
+                            <li><a href="opendagen.php">Open days</a></li>
+                            <li><a href="evenementen.php">Events</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="navFooterCenter">
+                        <ul>
+                            <li class="footerTopElement">Our partners:</li>
+                            <li><a href="https://www.nhlstenden.com/">NHL Stenden</a></li>
+                            <li><a href="https://www.drenthecollege.nl/">Drenthe College</a></li>
+                        </ul>
+                    </div>
+                    <div class="navFooterRight">
+                        <ul>
+                            <li class="footerTopElementSocial">Follow us:</li>
+                            <li><a href="https://www.facebook.com/"><img src="../image/facebook.png" alt="Facebook"/></a>
+                                <a href="https://twitter.com/"><img src="../image/twitter.png" alt="Twitter"/></a>
+                                <a href="https://youtube.com/"><img src="../image/youtube.png" alt="Youtube"/></a>
+                            </li>
+                            <li class="socialStyle">
+                                <a href="https://www.linkedin.com/"><img src="../image/linkedin.png" alt="Linkedin"/></a>
+                                <a href="https://instagram.com/"><img src="../image/instagram.png" alt="Instagram"/></a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>	
             </div>
-            <div class="mainFooter">
-                <div class="navFooterLeft">
-                    <ul>
-                        <li class="footerTopElement">T' Veenhoog:</li>
-                        <li><a href="opleidingen.php">Courses</a></li>
-                        <li><a href="opendagen.php">Open days</a></li>
-                        <li><a href="evenementen.php">Events</a></li>
-                        <li><a href="contact.php">Contact</a></li>
-                    </ul>
-                </div>
-                <div class="navFooterCenter">
-                    <ul>
-                        <li class="footerTopElement">Our partners:</li>
-                        <li><a href="https://www.nhlstenden.com/">NHL Stenden</a></li>
-                        <li><a href="https://www.drenthecollege.nl/">Drenthe College</a></li>
-                    </ul>
-                </div>
-                <div class="navFooterRight">
-                    <ul>
-                        <li class="footerTopElementSocial">Follow us:</li>
-                        <li><a href="https://www.facebook.com/"><img src="../image/facebook.png" alt="Facebook"/></a>
-                            <a href="https://twitter.com/"><img src="../image/twitter.png" alt="Twitter"/></a>
-                            <a href="https://youtube.com/"><img src="../image/youtube.png" alt="Youtube"/></a>
-                        </li>
-                        <li class="socialStyle">
-                            <a href="https://www.linkedin.com/"><img src="../image/linkedin.png" alt="Linkedin"/></a>
-                            <a href="https://instagram.com/"><img src="../image/instagram.png" alt="Instagram"/></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>	
+            <div class="rightBlok">
+                <a href="../nl/opleidingen.php">
+                    <div class="languageDutch">	
+                        <h3>Nederlands</h3>
+                    </div>
+                </a>
+                <a href="opleidingen.php">
+                    <div class="languageEnglish">
+                        <h3>English</h3>
+                    </div>
+                </a>
+                <?php
+                if (isset($_SESSION["authenticated"])) {
+                    if ($_SESSION["authenticated"]) {
+                        echo '<a href="signout.php"><div class="loginRight"><h3>Signout</h3></div></a>';
+                    } else {
+                        echo '<a href="login.php"><div class="loginRight"><h3>Login</h3></div></a>';
+                    }
+                } else {
+                    $_SESSION["authenticated"] = false;
+                    if ($_SESSION["authenticated"]) {
+                        echo '<a href="signout.php"><div class="loginRight"><h3>Signout</h3></div></a>';
+                    } else {
+                        echo '<a href="login.php"><div class="loginRight"><h3>Login</h3></div></a>';
+                    }
+                }
+                ?>
+            </div>
         </div>
-        <div class="rightBlok">
-            <a href="../nl/opleidingen.php">
-                <div class="languageDutch">	
-                    <h3>Nederlands</h3>
-                </div>
-            </a>
-            <a href="opleidingen.php">
-                <div class="languageEnglish">
-                    <h3>English</h3>
-                </div>
-            </a>
-            <a href="login.php">
-                <div class="loginRight">	
-                    <h3>Sign in</h3>
-                </div>
-            </a>
-        </div>
-    </div>
-</body>
+    </body>
 </html>
--- nl/opleidingen.php
+++ nl/opleidingen.php
@@ -14,10 +14,7 @@ session_start();
         <div class="logo">
             <a href="index.php"><img src="../image/logo.png" alt="logo"></a>
         </div>
-
-
         <div class="mainBody">
-
             <div class="mainHeader">
                 <div class="h1Title">
                     <h1>Technische Hogeschool 'T Veenhoog</h1>
@@ -26,34 +23,36 @@ session_start();
             <div class="mainopleiding">
                 <div class="h2title"><h2>Hier staat alles over onze opleidingen</h2></div>
                 <div class="box-opleiding">
-                    <div class="h2subtitle"><h2>Automotive</h2></div>
-                    <br>
+                    <div class="h2subtitle">
+                        <h2>Automotive</h2>
+                    </div>
                     <p>
                         De wereld van auto’s en techniek zit in een van zijn spannendste fasen ooit.
                         Onze toekomstige mobiliteit vraagt namelijk om oplossingen die revolutionair anders zijn.
-                    </p><br><p>
+                    </p>
+                    <p>
                         Denk alleen al aan de eindigheid van fossiele brandstoffen en de uitstoot van schadelijke stoffen.
                         In die toekomst leren we creatiever omgaan met grondstoffen, leren we anders kijken naar de veiligheid van elke weggebruiker en bedenken we oplossingen die de files terugdringen.
                         Het goede nieuws is dat de automotive wereld daar nu al op topsnelheid mee experimenteert.
                         Met slimme toepassingen van ICT, elektronica en lichtgewicht construeren, maar ook door toepassing van elektrische of hybride aandrijvingen.
-                    </p><br><p>
+                    </p>
+                    <p>
                         De helden in dat verhaal zijn de automotive engineers van 'T Veenhoog.
                         Het zijn vooral de probleemoplossers die verder kijken en begrijpen dat elke technische oplossing past in een complex plaatje
                         waarin mobiliteit nog slimmer, schoner, goedkoper en efficiënter moet.
                         ‘Compleet voertuig denken’ noemen we dat.
                         Dat leer je in 'T Veenhoog. Stap in en Drive your Future.
                     </p>
-
                     <a href="inschrijvenAutomotive.php">
                         <div class="inschrijvenButton">	
                             <h3>Inshrijven Automotive</h3>
                         </div>
                     </a>
                 </div>
-
                 <div class="box-opleiding">
-                    <div class="h2subtitle"><h2>Mechatronica</h2></div>
-                    <br>
+                    <div class="h2subtitle">
+                        <h2>Mechatronica</h2>
+                    </div>
                     <p>
                         Mechatronica is een multidisciplinaire combinatie van elektrotechniek, werktuigbouwkunde en technische informatica.
                         In dit vak ben je een logische denker én een echte doener.
@@ -61,8 +60,7 @@ session_start();
                         Denk daarbij aan een geautomatiseerde wasstraat, een slimme productierobot, of binnen de gezondheidszorg een geavanceerde rolstoel of vervangende robotarm.
                         De moderne mensheid kan niet meer zonder mechatronica en er is dan ook grote behoefte aan goedopgeleide mechatronica engineers.
                         Bij 'T Veenhoog leren we je de theorie en de praktijk kennen en we leiden je op tot een specialist in het systematisch ontwerpen van onder meer besturingstechnieken van robotgestuurde producten en machines.
-                    </p>
-                    
+                    </p>                    
                     <a href="inschrijvenMechatronica.php">
                         <div class="inschrijvenButton">	
                             <h3>Inshrijven Mechatronica</h3>
@@ -113,11 +111,22 @@ session_start();
                     <h3>English</h3>
                 </div>
             </a>
-            <a href="login.php">
-                <div class="loginRight">	
-                    <h3>Login</h3>
-                </div>
-            </a>
+            <?php
+                if (isset($_SESSION["authenticated"])) {
+                    if ($_SESSION["authenticated"]) {
+                        echo '<a href="signout.php"><div class="loginRight"><h3>Signout</h3></div></a>';
+                    } else {
+                        echo '<a href="login.php"><div class="loginRight"><h3>Login</h3></div></a>';
+                    }
+                } else {
+                    $_SESSION["authenticated"] = false;
+                    if ($_SESSION["authenticated"]) {
+                        echo '<a href="signout.php"><div class="loginRight"><h3>Signout</h3></div></a>';
+                    } else {
+                        echo '<a href="login.php"><div class="loginRight"><h3>Login</h3></div></a>';
+                    }
+                }
+                ?>
         </div>
     </div>
 </body>
